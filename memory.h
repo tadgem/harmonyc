130e69a146a0e0c56a2bc9e20752c4bf7d49a332
Tadge <98434757+tadgem@users.noreply.github.com>
2025-03-21 14:25:49 +0000

    initial commit
    - add SDL as submodule
    - add EASTL & mimalloc as in tree source
    - start testing out allocation patterns

---
+++ memory.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "primitives.h"
+#include "mimalloc.h"
+#include "EASTL/unordered_map.h"
+
+namespace harmony
+{
+	/// <summary>
+	/// Given a size, begin address & end address
+	/// allow allocation of any given type, 
+	/// reusing previously freed locations
+	/// </summary>
+	struct FixedRangeAllocator
+	{
+		const uint64	mSize;
+		const void*		mBegin;
+		const void*		mEnd;
+		
+		void*			mHead = 0;
+	};
+
+	/// <summary>
+	/// Global struct creating memory and 
+	/// allowing creation of sub allocators for specific use cases
+	/// </summary>
+	struct Memory
+	{
+		uint64		mAllocatedBytes;
+		uint8*		mMemory;
+
+		template<uint64 MemorySize>
+		static	Memory		Create()
+		{
+			return Memory
+			{
+				.mAllocatedBytes = MemorySize,
+				.mMemory = static_cast<uint8*>(mi_malloc(MemorySize))
+			};
+		}
+	};
+}
