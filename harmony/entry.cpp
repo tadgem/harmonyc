f45d10d30a5a5d592d647df525c5a46cd78375b6
Tadge <98434757+tadgem@users.noreply.github.com>
2025-04-02 18:10:21 +0100

    update entry changes as lvk state is now unique

--- harmony/entry.cpp
+++ harmony/entry.cpp
@@ -65,15 +65,15 @@ int main() {
     OnImGui(engine);
 
     // Game Loop
-    lvk::commands::RecordGraphicsCommands(engine.mVK, [&](VkCommandBuffer &cmd,
+    lvk::commands::RecordGraphicsCommands(*engine.mVK, [&](VkCommandBuffer& cmd,
                                                           uint32 frame) {
       lvk::Array<VkClearValue, 2> clearValues{};
       clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
       clearValues[1].depthStencil = {1.0f, 0};
-      VkTech::ClearRenderPass(engine.mVK, cmd, frame,
-                              engine.mVK.m_SwapchainImageRenderPass,
-                              engine.mVK.m_SwapChainFramebuffers[frame],
-                              engine.mVK.m_SwapChainImageExtent, clearValues);
+      VkTech::ClearRenderPass(*engine.mVK, cmd, frame,
+                              engine.mVK->m_SwapchainImageRenderPass,
+                              engine.mVK->m_SwapChainFramebuffers[frame],
+                              engine.mVK->m_SwapChainImageExtent, clearValues);
     });
 
     engine.EndFrame();
